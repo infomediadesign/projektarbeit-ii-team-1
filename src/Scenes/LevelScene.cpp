6389e9ea7fad663c60eca98d5a6a47f0ab083c9a
TokdilliS <sefer1994@live.de>
2022-07-08 17:39:27 +0200

    Nlohman added
    NLohmann JSON added
    Nlohman included in Level01
    Assets Directories added

--- src/Scenes/LevelScene.cpp
+++ src/Scenes/LevelScene.cpp
@@ -1,5 +1,41 @@
-//
-// Created by Munchkin on 24.05.2022.
-//
-
 #include "LevelScene.h"
+
+// JSON tilemap description
+//std::ifstream tilesetDescriptionFile("assets/maps/TilesetDescriptionFile.json");
+//nlohmann::json tilesetDescription = nlohmann::json::parse(tilesetDescriptionFile);
+//tilesetDescriptionFile.close();
+
+std::ifstream levelMapFile("assets/maps/Floor_1_in_Tiles.json");
+
+nlohmann::json levelMap = nlohmann::json::parse(levelMapFile);
+levelMapFil::close();
+
+//Texture2D tileAtlasTexture = LoadTexture((tilesetDescription["image"].get<std::string>()).c_str());
+//Map
+void LevelScene::DrawMap()
+{
+
+    Vector2 vec = {0, 0};
+    Rectangle rec = {0, 0, levelMap["width"], levelMap["height"]};
+
+    for (auto const &layer : levelMap["layers"]) {
+        if (layer["type"] == "layer" && layer["visible"]) {
+            for (auto const &tileId : layer["data"]) {
+                if (tileId != 0) {
+                    rec.x = (float) ((int) tileId - 1 % (int) tilesetDescription["columns"]) *
+                            (float) levelMap["width"];
+                    rec.y = (float) floor((float) tileId / (float) tilesetDescription["columns"]) *
+                            (float) levelMap["width"];
+
+                    DrawTextureRec(tileAtlasTexture, rec, vec, WHITE);
+                }
+
+                vec.x += (float) levelMap["width"];
+                if (vec.x >= (float) layer["width"] * (float) levelMap["width"]) {
+                    vec.x = 0;
+                    vec.y += (float) levelMap["height"];
+                }
+            }
+        }
+    }
+}
--- src/Scenes/LevelScene.h
+++ src/Scenes/LevelScene.h
@@ -1,13 +1,20 @@
-//
-// Created by Munchkin on 24.05.2022.
-//
-
 #ifndef RAYLIBSTARTER_LEVELSCENE_H
 #define RAYLIBSTARTER_LEVELSCENE_H
 
 #include "Scenes.h"
 
+#include <nlohmann/json.hpp>
+#include <fstream>
+
+
 class LevelScene : Scenes {
+    private:
+    ;
+    public:
+    void DrawMap();
+    ;
+    protected:
+    ;
 
 };
 
