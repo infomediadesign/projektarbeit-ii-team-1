aacf6913e737b6df3fb2bcd4d61e4faeab4578c6
Munchkin1327 <11016417@STUD.HOCHSCHULE-HEIDELBERG.DE>
2022-09-01 11:06:59 +0200

    added classes for TitleScreen and MainOptions; cleaned up main.cpp a bit

---
+++ src/Scenes/TitleScreen.cpp
@@ -0,0 +1,61 @@
+//
+// Created by Munchkin on 31.08.2022.
+//
+
+#include "TitleScreen.h"
+#include <vector>
+
+TitleScreen::TitleScreen()
+{
+    //Logo Image
+    Image titleScreen = LoadImage("assets/graphics/ui/Logo02.png");
+    ImageResize(&titleScreen, 500, 500);
+    Texture2D logo = LoadTextureFromImage(titleScreen);
+    UnloadImage(titleScreen);
+
+    //Text with font
+    this->font1 = LoadFont("assets/graphics/ui/Habbo.ttf");
+
+    Message1 = "Welcome to the Game!";
+    Message2 = "Please press Enter to continue.";
+
+    fontPosition1 = {GetScreenWidth()/2 -
+                     MeasureTextEx(font1, Message1.c_str(), (float)100, 1).x/2,
+                     GetScreenHeight() - (float)100/2 - 300};
+
+    fontPosition2 = {(GetScreenWidth()/2 + 450) -
+                     MeasureTextEx(font1, Message2.c_str(), (float)100, 1).x/2,
+                     GetScreenHeight() - (float)100/2 - 250};
+
+    switchScene = false;
+}
+
+TitleScreen::~TitleScreen()
+{
+
+}
+
+void TitleScreen::CustomUpdate()
+{
+    if (IsKeyPressed(KEY_ENTER))
+    {
+        this->switchTo = MAINMENU;
+        this->switchScene = true;
+    }
+}
+
+void TitleScreen::CustomDraw()
+{
+    //Textures
+    DrawTexture(logo, (GetScreenWidth() - logo.width)/2, (GetScreenHeight() - logo.height)/4, WHITE); //2 ursprünglich
+
+    //Messages
+    DrawTextEx(font1, Message1.c_str(), fontPosition1, 100, 1, WHITE);
+    DrawTextEx(font1, Message2.c_str(), fontPosition2, 50, 1, WHITE);
+}
+
+void TitleScreen::Unload()
+{
+    UnloadFont(font1);
+    UnloadTexture(logo);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 ﻿#include <cstdlib>
 
 #include "raylib.h"
-
 #include "config.h"
 
 //This is a test
@@ -17,6 +16,8 @@
 
 #include "Systems/DialogueManager.h"
 
+#include "Scenes/TitleScreen.h"
+#include "Scenes/MainOptions.h"
 #include "Scenes/BattleScene.h"
 #include "Scenes/MainMenuScene.h"
 #include "Scenes/PauseScene.h"
@@ -26,6 +27,7 @@
 #include "Scenes/ShopBarkeeper.h"
 #include "Scenes/ShopDealer.h"
 #include "Scenes/SkillTreeScene.h"
+
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -44,44 +46,17 @@ int main() {
     ToggleFullscreen();
 #endif
 
-    // Your own initialization code here
-    GameScreen currentScreen = TITLESCREEN;
-
-    //Titlescreen
-    Image titleScreen = LoadImage("assets/graphics/ui/Logo02.png");
-    ImageResize(&titleScreen, 500, 500);
-    Texture2D logo = LoadTextureFromImage(titleScreen);
-    UnloadImage(titleScreen);
-
-    LevelScene testL;
-
-    //Messages
-    //for Titlescreen
-    const char msg1[100] = "Welcome to the Game!";
-    const char msg2[100] = "Please press Enter to continue.";
-
-    //for Options
-    const char msg4[100] = "Options";
-
-    //for Credits
-
     //Font
     Font font1 = LoadFont("assets/graphics/ui/Habbo.ttf");
 
-    //Messages TitleScreen
-    Vector2 fontPosition1 = {Game::ScreenWidth/2 - MeasureTextEx(font1, msg1,
-                                                                 (float)font1.baseSize, 1).x/2, Game::ScreenHeight - (float)font1.baseSize/2 - 300};
-    Vector2 fontPosition2 = {Game::ScreenWidth/2 - MeasureTextEx(font1, msg2,
-                                                                 (float)font1.baseSize, 1).x/2, Game::ScreenHeight - (float)font1.baseSize/2 - 250};
-
-    //Messages OptionScreen
-    Vector2 fontPosition4 = {Game::ScreenWidth/2 - MeasureTextEx(font1, msg4,
-                                                                 (float)font1.baseSize, 1).x/2, Game::ScreenHeight - (float)font1.baseSize/2 - 250};
-
     //Implementing scenes
+    TitleScreen testTitle;
     MainMenuScene testMain;
+    MainOptions testMainOps;
     CreditScene testCredit;
     PauseScene testPause;
+
+    LevelScene testL;
 
 
     // Level initialisation goes here (use shared pointers!)
@@ -91,7 +66,7 @@ int main() {
     std::shared_ptr<LevelScene> levelRooftop;
 
     // Scene management
-    std::shared_ptr<Scenes> activeScene = std::make_shared<MainMenuScene>();
+    std::shared_ptr<Scenes> activeScene = std::make_shared<TitleScreen>();
     std::shared_ptr<LevelScene> activeLevel;
 
     // ALL OF THIS IS FOR TEST PURPOSES
@@ -157,45 +132,34 @@ int main() {
         // ========== UPDATE ==========
 
         // Scene transition
-        if (activeScene->switchScene == true)
-        {
+        if (activeScene->switchScene) {
             activeScene->switchScene = false;
-            currentScreen = activeScene->switchTo;
 
-            switch (activeScene->switchTo)
-            {
-                case MAINMENU:
+            switch (activeScene->switchTo) {
+                case TITLESCREEN:
                 {
+                    activeScene = std::make_shared<TitleScreen>();
+                    break;
+                }
+
+                case MAINMENU: {
                     activeScene = std::make_shared<MainMenuScene>();
-                    //if(testMain.switchScene == true)
-                    //    currentScreen = GAME;
                     break;
                 }
 
-                case MAINOPTIONS:
-                {
-                    // Should be moved to scene update / into a class
-                    if(IsKeyPressed(KEY_ESCAPE))
-                    {
-                        currentScreen = MAINMENU;
+                case MAINOPTIONS: {
+                    if (IsKeyPressed(KEY_ESCAPE)) {
+                        activeScene->switchTo = MAINMENU;
                     }
                     break;
                 }
 
-
-                case CREDITS:
-                {
+                case CREDITS: {
                     activeScene = std::make_shared<CreditScene>();
-                    // Should be moved to scene update / into a class
-                    if(IsKeyPressed(KEY_ESCAPE))
-                    {
-                        currentScreen = MAINMENU;
-                    }
                     break;
                 }
 
-                case GAME:
-                {
+                case GAME: {
                     activeScene = activeLevel;
 
 
@@ -244,8 +208,7 @@ int main() {
 
                     activeScene = std::make_shared<ShopDealer>(player);
                     break;
-                case PAUSEMENU:
-                {
+                case PAUSEMENU: {
                     activeScene = std::make_shared<PauseScene>();
 
                     /* All of this has to go to PauseScene.Update()!
@@ -265,28 +228,23 @@ int main() {
                     break;
                 }
 
-                case PAUSEOPTIONS:
-                {
-                    if(IsKeyPressed(KEY_ESCAPE))
-                    {
-                        currentScreen = PAUSEMENU;
+                case PAUSEOPTIONS: {
+                    if (IsKeyPressed(KEY_ESCAPE)) {
+                        //currentScreen = PAUSEMENU;
                     }
                     break;
                 }
-                case TESTSCENE:
-                {
+                case TESTSCENE: {
                     // This is a test
 
                     break;
                 }
-                case INVENTORY:
-                {
-                   activeScene->switchScene = false;
-                   activeScene = std::make_shared<InventoryScene>(player);
-                   break;
+                case INVENTORY: {
+                    activeScene->switchScene = false;
+                    activeScene = std::make_shared<InventoryScene>(player);
+                    break;
                 }
-                case SKILLTHREE:
-                {
+                case SKILLTHREE: {
                     activeScene->switchScene = false;
                     activeScene = std::make_shared<SkillTreeScene>(player);
                     break;
@@ -295,29 +253,27 @@ int main() {
         }
 
         // Scene update
-        if (currentScreen != TITLESCREEN)
+        if (activeScene->switchTo != TITLESCREEN)
+        {
             // TEMPORARY?
-            if (currentScreen == TESTSCENE) {
+            if (activeScene->switchTo == TESTSCENE) {
 
                 // This is going to be moved to LevelScene::Update()
                 player->Update();
 
                 //TEST
-                if(IsKeyPressed(KEY_I))
-                {
+                if (IsKeyPressed(KEY_I)) {
                     activeScene->switchTo = INVENTORY;
                     activeScene->switchScene = true;
                 }
-                if(IsKeyPressed(KEY_C))
-                {
+                if (IsKeyPressed(KEY_C)) {
                     activeScene->switchTo = SKILLTHREE;
                     activeScene->switchScene = true;
                 }
                 //TEST
 
                 // Check if a shop has to be opened
-                if(player->openShopBarkeeper == true && player->dialogueManager.dialoguePlaying == false)
-                {
+                if (player->openShopBarkeeper == true && player->dialogueManager.dialoguePlaying == false) {
                     TraceLog(LOG_INFO, "Opening shop...");
                     player->openShopBarkeeper = false;
 
@@ -325,8 +281,7 @@ int main() {
                     activeScene->switchTo = SHOP_BARKEEPER;
                     activeScene->switchScene = true;
                 }
-                if(player->openShopDealer == true && player->dialogueManager.dialoguePlaying == false)
-                {
+                if (player->openShopDealer == true && player->dialogueManager.dialoguePlaying == false) {
                     TraceLog(LOG_INFO, "Opening shop...");
                     player->openShopDealer = false;
 
@@ -336,8 +291,7 @@ int main() {
                     activeScene->switchScene = true;
                 }
                 // Check if a fight has to be started
-                if(player->startCombat == true && player->dialogueManager.dialoguePlaying == false)
-                {
+                if (player->startCombat == true && player->dialogueManager.dialoguePlaying == false) {
                     TraceLog(LOG_INFO, "Starting combat...");
                     player->startCombat = false;
                     // Start combat with player and player->enemyToFight
@@ -353,11 +307,12 @@ int main() {
 
                 // Check enemy aggro radius collision (maybe move this into a method of the level-class
                 bool stopSearch = false;
-                for (int i = 0; i < enemies.size() && stopSearch == false && player->dialogueManager.dialoguePlaying == false; i++)
-                {
-                    if (CheckCollisionCircleRec({enemies[i]->position.x + enemies[i]->frameRec.width / 2, enemies[i]->position.y + enemies[i]->frameRec.height / 2},
-                                                enemies[i]->aggroRadius, player->collisionBox) && enemies[i]->defeated == false)
-                    {
+                for (int i = 0; i < enemies.size() && stopSearch == false &&
+                                player->dialogueManager.dialoguePlaying == false; i++) {
+                    if (CheckCollisionCircleRec({enemies[i]->position.x + enemies[i]->frameRec.width / 2,
+                                                 enemies[i]->position.y + enemies[i]->frameRec.height / 2},
+                                                enemies[i]->aggroRadius, player->collisionBox) &&
+                        enemies[i]->defeated == false) {
                         player->interactionForced(enemies[i]);
                         stopSearch = true;
                     }
@@ -380,27 +335,21 @@ int main() {
                 for (int i = 0; i < dealers.size(); i++) {
                     dealers[i]->Update();
                 }
-            } else {
-                {
-                    activeScene->Update();
-                }
             }
-        else
-        {
-            if(IsKeyPressed(KEY_ENTER))
+            else
             {
-                currentScreen = MAINMENU;
+                activeScene->Update();
             }
-        }
+    }
 
 
         // ========== DRAW ==========
         BeginDrawing();
         ClearBackground(BLACK);
 
-        if (currentScreen != TITLESCREEN) {
+        if (activeScene->switchTo != TITLESCREEN) {
             // TEMPORARY
-            if (currentScreen == TESTSCENE) {
+            if (activeScene->switchTo == TESTSCENE) {
                 levelTutorial->Draw();
 
                 for (int i = 0; i < actors.size(); i++) {
@@ -417,20 +366,15 @@ int main() {
                 }
                 // Draw player after NPCs
                 player->Draw();
-
-            } else {
-                if (activeScene->drawLevelBackground == true) {
+            }
+            else
+            {
+                if (activeScene->drawLevelBackground == true)
+                {
                     activeLevel->Draw();
                 }
                 activeScene->Draw();
             }
-        }
-        else
-        {
-            DrawTexture(logo, Game::ScreenWidth/2 - logo.width/2, Game::ScreenHeight/4 - logo.height/4, WHITE);
-
-            DrawTextEx(font1, msg1,fontPosition1, font1.baseSize, 1,WHITE);
-            DrawTextEx(font1, msg2,fontPosition2, font1.baseSize, 1,WHITE);
         }
 
         /*
@@ -504,10 +448,10 @@ int main() {
     } // Main game loop end
 
     // De-initialization here
-
-    UnloadTexture(logo);
     UnloadFont(font1);
+    testTitle.Unload();
     testMain.Unload();
+    testMainOps.Unload();
     testCredit.Unload();
     testPause.Unload();
 
