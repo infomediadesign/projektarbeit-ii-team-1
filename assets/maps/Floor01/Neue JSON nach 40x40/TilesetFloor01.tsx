9ae88ca22d3fd53e4306c503b4d61a81e63c0020
Marko Lapadatovic <100348179+Maheatrix@users.noreply.github.com>
2022-09-14 21:06:52 +0200

    Create TilesetFloor01.tsx

---
+++ assets/maps/Floor01/Neue JSON nach 40x40/TilesetFloor01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="TilesetFloor01" tilewidth="40" tileheight="40" tilecount="1260" columns="45">
+ <image source="../Documents/GitHub/projektarbeit-ii-team-1/assets/maps/Floor01/TilesetFloor01.png" width="1800" height="1120"/>
+</tileset>
